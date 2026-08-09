fb5cefecff3fac845a76d47a74848771aafe737f
Luis Tureo Pérez <l.tureop@gmail.com>
2026-08-09 07:48:56 -0400

    Ignorar caché generada de Puppeteer en ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,9 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist/**', 'coverage/**', 'node_modules/**', 'sources/**'] },
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**', 'sources/**', '.cache/**'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
